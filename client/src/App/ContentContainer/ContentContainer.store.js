1c176a2e2add459ddcb8d88ce732541ff4da57e4
Eva Stern-Rodriguez <esternro@wellesley.edu>
2017-10-13 14:57:50 -0400

    filtering abitlity 100

--- client/src/App/ContentContainer/ContentContainer.store.js
+++ client/src/App/ContentContainer/ContentContainer.store.js
@@ -1,15 +1,30 @@
 import { observable, action } from "mobx";
+import results from './ResultPage/results.json';
 
 export default class ContentContainerStore {
   @observable content = 'landing';
+  @observable filter_keywords = '';
+  @observable displayed_results = results;
+
+  @action filter_results() {
+  	console.log(this.filter_keywords);
+  	console.log("HELLOOOOOOOO");
+  	console.log(this.filter_keywords);
+  	// filter the displayed results by the text
+  	this.displayed_results = results.filter(function (result) {
+    	return (result['Name'].includes(this.filter_keywords) || result['Description'].includes(this.filter_keywords));
+	}, this);
+  }
+
+  @action update_text(text) {
+  	this.filter_keywords = text;
+  }
 
   @action start() {
-  	console.log("STARTING")
     this.content = 'background';
   }
 
   @action choose_background() {
-  	console.log("I AM GETTIN MY RESULTSSSSS")
   	this.content = 'results';
   }
 }
--- client/src/App/ContentContainer/ResultPage/ResultPage.js
+++ client/src/App/ContentContainer/ResultPage/ResultPage.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
 import Select from 'react-select';
-import results from './results.json';
 
 import { Card, Button, CardImg, CardTitle, CardText, CardDeck, CardSubtitle, CardBody } from 'reactstrap';
 
@@ -12,19 +11,26 @@ import './ResultPage.css';
 @inject("store")
 @observer
 class ResultPage extends Component {
-
+    update_text = (event) => {
+        this.props.store.update_text(event.target.value)
+    }
+    onClick = () =>{
+    	this.props.store.filter_results()
+    }
 	render() { 
+		console.log(this.props.store.displayed_results)
 		return (
 			<div className="result-page">
 				<div className="filterbox">
 					<Card>
 						<CardTitle>Filter your results</CardTitle>
-						<Button>Filter</Button>
+						<input type="text" onChange={this.update_text} value={this.props.store.filter}/>
+						<Button onClick={this.onClick}>Filter</Button>
 					</Card>
 				</div>
 				
 				<div className="results">
-					{ results.map((result)=>{
+					{ this.props.store.displayed_results.map((result)=>{
 					return <Card>
 						<CardTitle>{result['Name']}</CardTitle>
 						<CardText>{result['Description']}</CardText>
