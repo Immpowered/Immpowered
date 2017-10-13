ae2327900a15365fbd5840bd0a9dd6e006063249
Eva Stern-Rodriguez <esternro@wellesley.edu>
2017-10-12 21:57:50 -0400

    read csv file

--- server/app.py
+++ server/app.py
@@ -1,9 +1,18 @@
-from flask import Flask, send_from_directory
+import pandas
+from flask import Flask, send_from_directory, render_template
+
 app = Flask(__name__, static_folder='build')
 
 @app.route('/')
 def index():
 	return render_template(app.static_folder+'/index.html')
+
+@app.route('/programs/', methods=['GET'])
+def programs():
+	data = pandas.read_csv('./data/programs.csv')
+	print(data)
+	return data.to_json(orient='records')
+
 
 @app.route('/<path:filename>')
 def upload(filename):
