3742389301f9babc2d4d89da71bd3f5b60a3832e
James <jamesleung417@gmail.com>
2017-11-16 08:56:31 -0500

    Fixed Result Cards

--- client/src/App/Content/Results/Results.js
+++ client/src/App/Content/Results/Results.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
-import { Card, CardTitle, CardText, CardMedia } from 'material-ui/Card';
-import RaisedButton from 'material-ui/RaisedButton';
+import { Card, CardTitle, CardText, CardActions } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import Timeline from '../Timeline/Timeline';
 import './Results.css';
+import ResultStore from './Results.Store';
 
 @inject("store")
 @observer
@@ -16,14 +18,14 @@ export default class Results extends Component {
         const subtext = "Brooklyn Workforce Innovations has eight specialized training programs for different job sectors. You’ll get hands-on experience and job placement support for up to two years after graduation. Not sure where to look first? Answer these three questions and we can help you get started on your search for the right program."
         return (
             <div className="results">
-                <h1> Results </h1>
+                <Timeline />
                 <Card style={card_style}>
-                    <CardMedia
-                        overlay={<CardTitle title="Brooklyn Workforce Innovations" subtitle={"BWI helps jobless and working poor New Yorkers establish careers."} />}
-                    >
-                        <img src={"https://bwiny.org/wp-content/uploads/2016/10/apply-slide1.jpg"} alt="Brooklyn Workforce Innovations" />
-                    </CardMedia>
-                    <p>{subtext}</p>
+                    <CardTitle title="Brooklyn Workforce Innovations" subtitle={"BWI helps jobless and working poor New Yorkers establish careers."} />
+                    <CardText>{subtext}</CardText>
+                    <CardActions>
+                        <FlatButton label="Prev" primary={true} onClick={ResultStore.next_result} />
+                        <FlatButton label="Next" primary={true} onClick={ResultStore.next_result} />
+                    </CardActions>
                 </Card>
             </div>
         );
