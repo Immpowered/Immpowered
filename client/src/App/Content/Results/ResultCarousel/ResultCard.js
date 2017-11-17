1488e8f56cad93dce9ab557d6a31fe71374e1d69
James Leung <leungJ.417@gmail.com>
2017-11-17 13:28:17 -0500

    Visit button

--- client/src/App/Content/Results/ResultCarousel/ResultCard.js
+++ client/src/App/Content/Results/ResultCarousel/ResultCard.js
@@ -8,18 +8,25 @@ import { Carousel } from 'react-responsive-carousel';
 
 @observer
 export default class ResultCard extends Component {
-    render() {
-        console.log("I AM RENDERING: ", this.props)
+    openInNewTab = (url) => {
+        return () => {
+            var win = window.open('http://'+url, '_blank');
+            win.focus();
+        }
+    }
+
+    render() {    
         const card_style = {
                 transitionDuration: '0.3s',
                 height: '100%'
             }
+
         return (          
             <Card style={card_style}>
                 <CardTitle title={this.props.name} subtitle={this.props.url} />
                 <CardText>{this.props.description}</CardText>
                 <CardActions>
-                    <FlatButton label="Visit Website" primary={true} onClick={this.props.prev_fn} />
+                    <FlatButton label="Visit Website" primary={true} onClick={this.openInNewTab(this.props.url)} />
                 </CardActions>
             </Card>
         );
