3de7141df4a1dc6652c048059f4758eb0aff617d
pytseng <tw.pytseng@gmail.com>
2017-11-16 20:35:50 -0500

    fix style for carousel and splash height

--- client/src/App/Content/Results/Results.js
+++ client/src/App/Content/Results/Results.js
@@ -15,8 +15,9 @@ export default class Results extends Component {
         const card_style = {
             transitionDuration: '0.3s',
             height: '100%',
-            width: '80%'
+            width: 400
         }
+
         const subtext = "Brooklyn Workforce Innovations has eight specialized training programs for different job sectors. You’ll get hands-on experience and job placement support for up to two years after graduation. Not sure where to look first? Answer these three questions and we can help you get started on your search for the right program."
         return (
             <div className="results">
